68924a2f05f02d8daa7ef2125e704727d1d8c038
teabeans <twhlum@gmail.com>
2020-03-15 19:28:29 -0700

    Application compile script running.

--- application/Main.java
+++ application/Main.java
@@ -3,18 +3,18 @@ public class Main {
     System.out.println( "Hi! I am a host/application!" );
     System.out.println( "I want to make a call to the HSM." );
     System.out.println( "I will send an application-specific request to the driver." );
-    driverWrite();
+    applicationWrite();
 
     System.out.println( "I can also read application-specific responses from the driver." );
-    driverRead();
+    applicationRead();
   }
 
-  public void driverRead( ) {
+  public static void applicationRead( ) {
     System.out.println( "This is a read of a message from a driver" );
     System.out.println( "Please read me to msgs/driver_to_application.txt" );
   }
 
-  public void driverWrite( ) {
+  public static void applicationWrite( ) {
   	System.out.println( "This is an application-specific message being sent to the driver" );
   	System.out.println( "Please write me to msgs/application_to_driver.txt" );
   }
