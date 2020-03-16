91304d2c65b4d8e884eb545ab262e8a24da6cf46
Tim Lum <twhlum@gmail.com>
2020-03-16 00:28:12 -0700

    Update Main.java

--- driver/Main.java
+++ driver/Main.java
@@ -38,4 +38,128 @@ public class Main {
     System.out.println( "This is a write of a message to a vHSM" );
     System.out.println( "Please write me to msgs/driver_to_vHSM.txt" );
   }
-}
+//-------|---------|---------|---------|---------|---------|---------|---------|
+//
+// SUPPORT FUNCTIONS
+//
+//-------|---------|---------|---------|---------|---------|---------|---------|
+
+//-------|---------|---------|---------|---------|---------|---------|---------|
+//
+// READERS/LOADERS
+//
+//-------|---------|---------|---------|---------|---------|---------|---------|
+
+//-------|---------|---------|---------|
+// driverRead()
+//-------|---------|---------|---------|
+  public static String driverReadFromApp( ) {
+    String result = readFileToString( READ_FILE );
+    return result;
+  }
+//-------|---------|---------|---------|
+// driverRead()
+//-------|---------|---------|---------|
+  public static String driverReadFromvHSM( ) {
+    String result = readFileToString( READ_FILE );
+    return result;
+  }
+
+//-------|---------|---------|---------|
+// driverWrite()
+//-------|---------|---------|---------|
+  public static void driverWriteToApp( String message ) {
+    writeStringToFile( message, WRITE_FILE );
+  }
+//-------|---------|---------|---------|
+// driverWrite()
+//-------|---------|---------|---------|
+  public static void driverWriteTovHSM( String message ) {
+    writeStringToFile( message, WRITE_FILE );
+  }
+
+//-------|---------|---------|---------|
+// readFileToString()
+//-------|---------|---------|---------|  
+// Reads a file and returns the contents as a String
+  public static String readFileToString( String filename ) {    
+    File f = new File( filename );
+    Scanner fileReader = null;
+    try {
+      fileReader = new Scanner( f );
+    }
+    catch (FileNotFoundException e) {
+      e.printStackTrace();
+    }
+    String retString = "";
+    while( fileReader.hasNextLine() ) {
+      retString += fileReader.nextLine();
+    }
+    if( DEBUG ) {
+      System.out.println( "  [readFile()] - Read File: " + filename );
+      System.out.println( "  [readFile()] - Contents : " );
+      System.out.println( retString );
+      System.out.println();
+    }
+    return retString;
+  } // Closing readFile()
+
+//-------|---------|---------|---------|
+// writeStringToFile()
+//-------|---------|---------|---------|  
+// Takes a String and writes to file
+  public static void writeStringToFile( String text, String filename ) {    
+    FileWriter fw = null;
+    BufferedWriter writer = null;
+    try {
+      fw = new FileWriter( filename );
+      writer = new BufferedWriter( fw );
+      writer.write( text );
+      writer.close();
+    }
+    catch (IOException e) {
+      e.printStackTrace();
+    }
+  } // Closing writeStringToFile()
+
+//-------|---------|---------|---------|---------|---------|---------|---------|
+//
+// RENDERERS
+//
+//-------|---------|---------|---------|---------|---------|---------|---------|
+
+//-------|---------|---------|---------|
+// renderOptions()
+//-------|---------|---------|---------|
+  public static void renderOptions() {
+    long delay = 10;
+    System.out.println( "+-------------------------------------------------------------------------------+" );
+    System.out.println( "|   OPTIONS                                                                     |" );
+    System.out.println( "+-------------------------------------------------------------------------------+" );
+    System.out.println( "|   \u001b[1mL\u001b[0m  - \u001b[4mL\u001b[0mogin                                                                  |" );
+    if( LOGGED_IN ) {
+      System.out.println( "|   \u001b[1mM\u001b[0m  - \u001b[4mM\u001b[0make Key                                                               |" );
+      System.out.println( "|   \u001b[1mR\u001b[0m  - \u001b[4mR\u001b[0mead Key                                                               |" );
+    }
+    else if( !LOGGED_IN ) {
+      System.out.println( "|   \u001b[30;1mM  - (Unavailable - Please log in) Make Key \u001b[0m                              |" );
+      System.out.println( "|   \u001b[30;1mR  - (Unavailable - Please log in) Read Key \u001b[0m                 |" );
+    }
+    System.out.println( "|   \u001b[1mV\u001b[0m  - \u001b[4mV\u001b[0merbosity (toggle)                                                     |" );
+    System.out.println( "|   \u001b[1mX\u001b[0m  - e\u001b[4mX\u001b[0mit                                                                   |" );
+    System.out.println( "+-------------------------------------------------------------------------------+" );
+    System.out.println();
+    System.out.print( "\u001b[37;1mPlease select an option: \u001b[0m" );
+  } // Closing renderOptions()
+
+//-------|---------|---------|---------|---------|---------|---------|---------|
+//
+// FORMAT CONVERSION
+//
+//-------|---------|---------|---------|---------|---------|---------|---------|
+
+// None yet identified
+
+} // Closing class Main
+
+// End of file - application/Main.java
